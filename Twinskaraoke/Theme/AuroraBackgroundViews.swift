fd6db2c7a14efdc471b4e4d12262d6ab4d737da6
SillyProotSoda <103301261+ytsodacan@users.noreply.github.com>
2026-07-29 17:25:02 +1000

    Create AuroraBackgroundViews.swift

---
+++ Twinskaraoke/Theme/AuroraBackgroundViews.swift
@@ -0,0 +1,141 @@
+import SwiftUI
+
+class CloudProvider: ObservableObject {
+    let offset: CGSize
+    let frameHeightRatio: CGFloat
+
+    init() {
+        frameHeightRatio = CGFloat.random(in: 0.7 ..< 1.4)
+        offset = CGSize(
+            width: CGFloat.random(in: -150 ..< 150),
+            height: CGFloat.random(in: -150 ..< 150)
+        )
+    }
+}
+
+struct Cloud: View {
+    @StateObject var provider = CloudProvider()
+    @State private var move = false
+
+    let proxy: GeometryProxy
+    let color: Color
+    let rotationStart: Double
+    let duration: Double
+    let alignment: Alignment
+
+    var body: some View {
+        Circle()
+            .fill(color)
+            .frame(height: proxy.size.height / provider.frameHeightRatio)
+            .offset(provider.offset)
+            .rotationEffect(.init(degrees: move ? rotationStart : rotationStart + 360))
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: alignment)
+            .opacity(0.8)
+            .onAppear {
+                withOptionalAnimation(Animation.linear(duration: duration).repeatForever(autoreverses: false)) {
+                    move.toggle()
+                }
+            }
+    }
+}
+
+struct FloatingClouds: View {
+    @Environment(\.colorScheme) var scheme
+    let blur: CGFloat = 60
+
+    var body: some View {
+        GeometryReader { proxy in
+            ZStack {
+                Theme.generalBackground
+                ZStack {
+                    Cloud(
+                        proxy: proxy,
+                        color: Theme.ellipsesBottomTrailing(forScheme: scheme),
+                        rotationStart: 0,
+                        duration: 60,
+                        alignment: .bottomTrailing
+                    )
+                    Cloud(
+                        proxy: proxy,
+                        color: Theme.ellipsesTopTrailing(forScheme: scheme),
+                        rotationStart: 240,
+                        duration: 50,
+                        alignment: .topTrailing
+                    )
+                    Cloud(
+                        proxy: proxy,
+                        color: Theme.ellipsesBottomLeading(forScheme: scheme),
+                        rotationStart: 120,
+                        duration: 80,
+                        alignment: .bottomLeading
+                    )
+                    Cloud(
+                        proxy: proxy,
+                        color: Theme.ellipsesTopLeading(forScheme: scheme),
+                        rotationStart: 180,
+                        duration: 70,
+                        alignment: .topLeading
+                    )
+                }
+                .blur(radius: blur)
+            }
+            .ignoresSafeArea()
+        }
+    }
+}
+
+struct LinearNonTransparency: View {
+    @Environment(\.colorScheme) var scheme
+
+    var gradient: Gradient {
+        Gradient(colors: [
+            Theme.ellipsesTopLeading(forScheme: scheme),
+            Theme.ellipsesTopTrailing(forScheme: scheme)
+        ])
+    }
+
+    var body: some View {
+        LinearGradient(gradient: gradient, startPoint: .topLeading, endPoint: .bottomTrailing)
+            .ignoresSafeArea()
+    }
+}
+
+func withOptionalAnimation<Result>(_ animation: Animation? = .default, _ body: () throws -> Result) rethrows -> Result {
+    #if canImport(UIKit)
+    if UIAccessibility.isReduceMotionEnabled {
+        return try body()
+    } else {
+        return try withAnimation(animation, body)
+    }
+    #else
+    return try withAnimation(animation, body)
+    #endif
+}
+
+// Reusable Modifier to apply background anywhere in your app
+struct AuroraBackgroundModifier: ViewModifier {
+    @Environment(\.accessibilityReduceTransparency) var reduceTransparency
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    @Environment(\.colorScheme) var scheme
+
+    func body(content: Content) -> some View {
+        ZStack {
+            if differentiateWithoutColor {
+                Theme.differentiateWithoutColorBackground(forScheme: scheme)
+                    .ignoresSafeArea()
+            } else if reduceTransparency {
+                LinearNonTransparency()
+            } else {
+                FloatingClouds()
+            }
+
+            content
+        }
+    }
+}
+
+extension View {
+    func auroraBackground() -> some View {
+        self.modifier(AuroraBackgroundModifier())
+    }
+}
